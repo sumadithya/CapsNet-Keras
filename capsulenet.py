560e40868cc545dc768abcee2bbf8e8fa87c1834
sumadithya <sumadithya@gmail.com>
2022-02-05 19:11:57 +0530

    simplied and changed manipulate latent

--- capsulenet.py
+++ capsulenet.py
@@ -12,8 +12,6 @@ Usage:
 Result:
     Validation accuracy > 99.5% after 20 epochs. Converge to 99.66% after 50 epochs.
     About 110 seconds per epoch on a single GTX1070 GPU card
-    
-Author: Xifeng Guo, E-mail: `guoxifeng1990@163.com`, Github: `https://github.com/XifengGuo/CapsNet-Keras`
 """
 
 import numpy as np
@@ -147,7 +145,6 @@ def train(model,  # type: models.Model
 
     return model
 
-
 def test(model, data, args):
     x_test, y_test = data
     y_pred, x_recon = model.predict(x_test, batch_size=100)
@@ -163,32 +160,6 @@ def test(model, data, args):
     plt.imshow(plt.imread(args.save_dir + "/real_and_recon.png"))
     plt.show()
 
-
-def manipulate_latent(model, data, args):
-    print('-' * 30 + 'Begin: manipulate' + '-' * 30)
-    x_test, y_test = data
-    index = np.argmax(y_test, 1) == args.digit
-    number = np.random.randint(low=0, high=sum(index) - 1)
-    x, y = x_test[index][number], y_test[index][number]
-    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
-    noise = np.zeros([1, 10, 16])
-    x_recons = []
-    for dim in range(16):
-        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
-            tmp = np.copy(noise)
-            tmp[:, :, dim] = r
-            x_recon = model.predict([x, y, tmp])
-            x_recons.append(x_recon)
-
-    x_recons = np.concatenate(x_recons)
-
-    img = combine_images(x_recons, height=16)
-    image = img * 255
-    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
-    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
-    print('-' * 30 + 'End: manipulate' + '-' * 30)
-
-
 def load_mnist():
     # the data, shuffled and split between train and test sets
     from tensorflow.keras.datasets import mnist
@@ -199,7 +170,6 @@ def load_mnist():
     y_train = to_categorical(y_train.astype('float32'))
     y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
-
 
 if __name__ == "__main__":
     import os
@@ -254,5 +224,4 @@ if __name__ == "__main__":
     else:  # as long as weights are given, will run testing
         if args.weights is None:
             print('No weights are provided. Will test using random initialized weights.')
-        manipulate_latent(manipulate_model, (x_test, y_test), args)
         test(model=eval_model, data=(x_test, y_test), args=args)
---
+++ manipulate_latent.py
@@ -0,0 +1,79 @@
+def manipulate_latent(model, data, args):
+    print('-' * 30 + 'Begin: manipulate' + '-' * 30)
+    x_test, y_test = data
+    index = np.argmax(y_test, 1) == args.digit
+    number = np.random.randint(low=0, high=sum(index) - 1)
+    x, y = x_test[index][number], y_test[index][number]
+    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
+    noise = np.zeros([1, 10, 16])
+    x_recons = []
+    for dim in range(16):
+        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
+            tmp = np.copy(noise)
+            tmp[:, :, dim] = r
+            x_recon = model.predict([x, y, tmp])
+            x_recons.append(x_recon)
+
+    x_recons = np.concatenate(x_recons)
+
+    img = combine_images(x_recons, height=16)
+    image = img * 255
+    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
+    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
+    print('-' * 30 + 'End: manipulate' + '-' * 30)
+
+if __name__ == "__main__":
+    import os
+    import argparse
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    from tensorflow.keras import callbacks
+
+    # setting the hyper parameters
+    parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--batch_size', default=100, type=int)
+    parser.add_argument('--lr', default=0.001, type=float,
+                        help="Initial learning rate")
+    parser.add_argument('--lr_decay', default=0.9, type=float,
+                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
+    parser.add_argument('--lam_recon', default=0.392, type=float,
+                        help="The coefficient for the loss of decoder")
+    parser.add_argument('-r', '--routings', default=3, type=int,
+                        help="Number of iterations used in routing algorithm. should > 0")
+    parser.add_argument('--shift_fraction', default=0.1, type=float,
+                        help="Fraction of pixels to shift at most in each direction.")
+    parser.add_argument('--debug', action='store_true',
+                        help="Save weights by TensorBoard")
+    parser.add_argument('--save_dir', default='./result')
+    parser.add_argument('-t', '--testing', action='store_true',
+                        help="Test the trained model on testing dataset")
+    parser.add_argument('--digit', default=5, type=int,
+                        help="Digit to manipulate")
+    parser.add_argument('-w', '--weights', default=None,
+                        help="The path of the saved weights. Should be specified when testing")
+    args = parser.parse_args()
+    print(args)
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    # load data
+    (x_train, y_train), (x_test, y_test) = load_mnist()
+
+    # define model
+    model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
+                                                  n_class=len(np.unique(np.argmax(y_train, 1))),
+                                                  routings=args.routings,
+                                                  batch_size=args.batch_size)
+    model.summary()
+
+    # train or test
+    if args.weights is not None:  # init the model weights with provided one
+        model.load_weights(args.weights)
+    if not args.testing:
+        train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
+    else:  # as long as weights are given, will run testing
+        if args.weights is None:
+            print('No weights are provided. Will test using random initialized weights.')
+        manipulate_latent(manipulate_model, (x_test, y_test), args)
+        test(model=eval_model, data=(x_test, y_test), args=args)
